b6b8adea678b5031246f0d357ecefdd2b4eaaf7c
Tara Ellicott <taraellicott@gmail.com>
2018-04-11 18:35:02 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -13,3 +13,13 @@ end
 fizzbuzz(3)
 fizzbuzz(5)
 fizzbuzz()
+
+# def fizzbuzz(int)
+#   if int % 3 == 0 && int % 5 == 0
+#     "FizzBuzz"
+#   elsif int % 3 == 0
+#     "Fizz"
+#   elsif int % 5 == 0
+#     "Buzz"
+#   end
+# end
